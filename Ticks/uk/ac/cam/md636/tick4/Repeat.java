61a3881ff7d62fcacd0ebb4af1386b332f4d7a4c
ascent95 <matthewjduffin@gmail.com>
2014-02-05 22:49:04 +0000

    I'm pretty sure I definitely did fix Repeat.java this time.

--- Ticks/uk/ac/cam/md636/tick4/Repeat.java
+++ Ticks/uk/ac/cam/md636/tick4/Repeat.java
@@ -1,31 +1,32 @@
 package uk.ac.cam.md636.tick4;
 
 public class Repeat {
-	public static void main(String[] args) {
-		System.out.println(parseAndRep(args));
-	}
-	public static String parseAndRep(String[] args) {
-		if (args.length != 2) {
-			return "Error: insufficient arguments";
-		}
-		int rep = Integer.parseInt(args[1]);
-		if (rep < 1) {
-		    return "Error: second argument is not a positive integer";
-		}
-		try {
-			String repeated = "";
-			for (int i = 0; i < rep; i++) {
-				if (i == rep - 1) {
-					repeated += args[0]; // Prevents an extra space being placed
-											// on the end. somechange
-				} else {
-					repeated += (args[0] + " ");
-				}
-			}
-			return repeated;
-		} catch (NumberFormatException error) {
-			return "Error: second argument is not a positive integer";
-		}
+    public static void main(String[] args) {
+        System.out.println(parseAndRep(args));
+    }
 
-	}
+    public static String parseAndRep(String[] args) {
+        if (args.length != 2) {
+            return "Error: insufficient arguments";
+        }
+        try {
+            int rep = Integer.parseInt(args[1]);
+            if (rep < 1) {
+                return "Error: second argument is not a positive integer";
+            }
+            String repeated = "";
+            for (int i = 0; i < rep; i++) {
+                if (i == rep - 1) {
+                    repeated += args[0]; // Prevents an extra space being placed
+                                         // on the end. somechange
+                } else {
+                    repeated += (args[0] + " ");
+                }
+            }
+            return repeated;
+        } catch (NumberFormatException error) {
+            return "Error: second argument is not a positive integer";
+        }
+
+    }
 }
